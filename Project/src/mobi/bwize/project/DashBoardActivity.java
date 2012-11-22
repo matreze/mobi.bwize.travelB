838434404b6cf762c40e9957f03f538718f15e97
Michael Reid <matreze@gmail.com>
2012-11-22 18:57:43 +0000

    Added code for to-do list fragment and click handler for to-do button

--- Project/src/mobi/bwize/project/DashBoardActivity.java
+++ Project/src/mobi/bwize/project/DashBoardActivity.java
@@ -3,21 +3,34 @@ package mobi.bwize.project;
 import java.sql.SQLException;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class DashBoardActivity extends FragmentActivity {
 
+	ImageButton backButton, viewButton;
 	TextView txtDest;
 	String details[];
+	String destination[];
+	String departure[];
 	String cityParsed;
 	String countryParsed;
+	String trip;
 
 	/*
 	 * String[] columns = new String[] { KEY_TRIP_ID, 
@@ -43,6 +56,7 @@ public class DashBoardActivity extends FragmentActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
 		FragmentManager fragmentManager = getSupportFragmentManager();
 
@@ -50,6 +64,8 @@ public class DashBoardActivity extends FragmentActivity {
 
 		// check the string from the previous activity
 		String message = intent.getStringExtra("ID");
+		int id=Integer.valueOf(message);
+		trip=message;
 
 		Trip_Database db = new Trip_Database(this);
 
@@ -61,21 +77,33 @@ public class DashBoardActivity extends FragmentActivity {
 		}
 
 		// Get the destination name from the last activity to populate the
+
+		// Get the destination name from the last activity to populate the
 		details = db.getTripDetails(Integer.valueOf(message));
+		destination = db.get_Destination(id);
+		departure = db.get_ReturnInfo(id);
 
-		cityParsed = details[2].replaceAll("\\s", "%20");
-		countryParsed = details[1].replaceAll("\\s", "%20");
+		cityParsed = destination[1].replaceAll("\\s", "%20");
+		countryParsed = destination[0].replaceAll("\\s", "%20");
 
-		CountDown countDownFragment = (CountDown) fragmentManager
-				.findFragmentById(R.id.frag_1);
-		countDownFragment.countDown(details[4], details[5]);
-
+		
 		CurrentWeather currentWeatherDownFragment = (CurrentWeather) fragmentManager
 				.findFragmentById(R.id.frag_2);
 		currentWeatherDownFragment.setCurrentWeather(countryParsed, cityParsed);
+		
+		ToDO toDoFragment = (ToDO) fragmentManager
+				.findFragmentById(R.id.frag_3);
+		toDoFragment.setID(message);
+		
+		CountDown countDownFragment = (CountDown) fragmentManager
+				.findFragmentById(R.id.frag_1);
+		countDownFragment.countDown(db.get_DepartureDate(id), db.get_DepartureTime(id));
+
 
 		db.close();
-
+		
+		ScrollView sview=(ScrollView) this.findViewById(R.id.scrollview);
+		sview.smoothScrollTo(0,0);
 		/*
 		 * String destData;
 		 * 
@@ -84,6 +112,39 @@ public class DashBoardActivity extends FragmentActivity {
 		 */
 
 		// txtDest.setText(details[2]);
+		
+		backButton = (ImageButton)this.findViewById(R.id.imageButton_back);
+		backButton.setOnClickListener(new OnClickListener() {
+		  //@Override
+		  public void onClick(View v) {
+		    finish();
+		  }
+		});
+
+	}
+	
+	public void ViewDetails(View view) {
+
+		Intent ViewDetails = new Intent(this, ViewDetails.class);
+
+		//To do 
+		ViewDetails.putExtra("City", destination[1]);
+		ViewDetails.putExtra("DepDate", details[4]);
+		ViewDetails.putExtra("DepFlightNo", details[3]);
+		ViewDetails.putExtra("DepTime", details[5]);
+		ViewDetails.putExtra("DepConnection", details[6]);
+		ViewDetails.putExtra("CountryDep", details[9]);
+		ViewDetails.putExtra("CityDep", details[10]);
+		ViewDetails.putExtra("CountryDest", details[1]);
+		ViewDetails.putExtra("CityDest", details[2]);
+		ViewDetails.putExtra("ReturnDate", details[12]);
+		ViewDetails.putExtra("ReturnFlightNo", details[11]);
+		ViewDetails.putExtra("ReturnTime", details[13]);
+		ViewDetails.putExtra("ReturnConnection", details[14]);
+		ViewDetails.putExtra("Notification", details[15]);
+		ViewDetails.putExtra("Comments", details[16]);
+
+		startActivity(ViewDetails);
 
 	}
 
@@ -102,8 +163,8 @@ public class DashBoardActivity extends FragmentActivity {
 
 		Intent currency = new Intent(this, CurrencyConv.class);
 
-		currency.putExtra("countryDest", details[1]);
-		currency.putExtra("countryDep", details[9]);
+		currency.putExtra("countryDest", destination[0]);
+		currency.putExtra("countryDep", departure[0]);
 
 		startActivity(currency);
 
@@ -123,6 +184,56 @@ public class DashBoardActivity extends FragmentActivity {
 					"No Intenet Connection Detected. Please Try Again Later", Toast.LENGTH_SHORT).show();
 			
 		}
+	}
+	
+	public void add(View view) {
+		 
+		View entryView;
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+				this);
+		entryView = getLayoutInflater().inflate(R.layout.new_task, null);
+		alertDialogBuilder.setView(entryView);
+		final EditText taskEditText = (EditText) entryView
+				.findViewById(R.id.task_text_xml);
+
+		// set the database helper
+		final ToDoDBHelper helper2 = new ToDoDBHelper(this);
+		// set title
+		alertDialogBuilder.setTitle("New Task");
+
+		// set dialog message
+		alertDialogBuilder
+				.setCancelable(true)
+				.setPositiveButton("Add",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								SQLiteDatabase database;
+								database = helper2
+										.getWritableDatabase();
+								database.execSQL("INSERT INTO tblToDo (trip,task,date) VALUES ('"
+										+ trip
+										+ "', '"
+										+ taskEditText.getText().toString()
+										+ "',"
+										+ (int) System.currentTimeMillis()
+										+ ")");
+								Log.d("set",taskEditText.getText().toString());
+								helper2.close();
+								ToDO.refresh();
+							}
+						})
+				.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						// if this button is clicked, just close
+						// the dialog box and do nothing
+						dialog.cancel();
+					}
+				});
+		// create alert dialog
+		AlertDialog alertDialog = alertDialogBuilder.create();
+		// show it
+		alertDialog.show();
+
 	}
 
 }
